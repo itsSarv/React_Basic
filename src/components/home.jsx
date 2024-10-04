415101de5de168a9c1d78dc863cc5f33b7466ded
saurav tiwari <sauravtiwari145@gmail.com>
2024-10-04 11:36:43 +0545

    change image

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Nav from '../components/nav'
 
 
+
 const home = () => {
   return (
     <div className='h-[100vh] min-w-full bg-[#181532]'>
       <Nav />
 
       {/* home image  */}
-      <img className='absolute right-0 top-0 z-10 h-[100vh] opacity-40  animate__animated  animate__zoomIn overflow-hidden ' src="../Assets/profile.png" alt="dfd" />
+      <img className='absolute right-0 top-0 z-10 h-[100vh] opacity-40  animate__animated  animate__zoomIn overflow-hidden ' src="./assets/profile.png" alt="dfd" />
 
       {/* home text */}
       <div className='m-0 flex flex-col h-[100vh] gap-10 justify-center pl-32 animate__animated animate__fadeInLeft'>
