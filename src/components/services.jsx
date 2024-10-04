e06eef352dc9092622f90e713e4328ee178c14b3
saurav tiwari <sauravtiwari145@gmail.com>
2024-10-04 15:35:42 +0545

    service page

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,61 +1,55 @@
 import React from 'react'
 import Nav from './nav'
-import { FaArrowRight, FaCropAlt, FaEdit, FaRegCheckCircle } from "react-icons/fa";
+
 
 
 const services = () => {
 
-    const services = [
+    const content = [
         {
-            icon: <FaCropAlt />,
-            name: 'Branding',
-            description: 'Lorem ipsum dolor sit consectetur adipisicing  elit Minus!'
+            image: './images/web.jpg',
+            content: 'Web Development',
+            description: ' Build and optimize your website with cutting-edge technologies and responsive design.'
         },
         {
-            icon: <FaEdit />,
-            name: 'Design',
-            description: 'Lorem ipsum dolor sit consectetur adipisicing elit Minus!'
+            image: './images/cloud.jpg',
+            content: 'Cloud Computing',
+            description: 'Secure and scalable cloud solutions tailored to your business needs.'
         },
         {
-            icon: <FaRegCheckCircle />,
-            name: 'Development',
-            description: 'Lorem ipsum dolor sit consectetur adipisicing elit Minus!'
-        }
+            image: './images/digital.jpg',
+            content: 'Digital Marketing',
+            description: 'Reach your audience with strategic campaigns across digital channels.'
+        },
     ]
 
     return (
-        <div className="flex lg:flex-row flex-col  justify-center items-center min-h-screen w-screen md:gap-16 gap-4   text-white bg-[#181532]">
-
+        <section className="text-white  relative bg-[#181532] h-[100vh] md:block flex justify-center overflow-hidden">
             <Nav />
 
-            {/* Left Section */}
-            <div className="animate__animated animate__fadeInDown">
-                <h1 className="text-4xl md:mb-4 md:mt-0 mt-16 ">
-                    My <span className="text-red-600">Work</span>.
-                </h1>
-                <p className="text-sm text-white/70">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum <br /> eius sapiente numquam a. Sed, odio est quidem officiis error, ex <br /> delectus eum repudiandae corporis quia cupiditate.
-                </p>
-            </div>
-
-            {/* Right Section */}
-            <div className="flex lg:justify-normal md:flex-row flex-col lg:px-0 px-6 justify-evenly lg:gap-8 gap-4 text-white/70 md:w-[60%] animate__animated animate__fadeInDown">
-                {services.map((item, index) => {
-                    return <div className="branding bg-white/5 md:p-8 p-4 hover:bg-white/10">
-                        <div className=" text-red-600 text-xl hover:text-white md:my-4 my-2">{item.icon}</div>
-                        <h2 className="text-xl mb-4">{item.name}</h2>
-                        <p>{item.description}</p>
-                        <div className=" text-red-600 text-xl hover:text-white md:mt-4 mt-2"> <FaArrowRight /> </div>
-                    </div>
-                })}
-
-
+            <section className="w-full flex flex-col justify-center items-center px-8 md:px-12">
+                <h2 className="text-4xl font-bold mb-14 md:mt-0 mt-20 text-center sticky top-20">My Services</h2>
+                <div className="flex flex-col md:flex-row justify-center gap-6">
+                    {content.map((item, index) => {
+                        return <div key={index} className="group relative overflow-hidden w-full md:w-1/3 rounded-lg shadow-lg bg-white">
+                            <img
+                                src={item.image}
+                                alt={item.content}
+                                className="w-full h-64 object-cover group-hover:scale-110 transition-transform duration-300"
+                            />
+                            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                <h3 className="text-white text-xl font-semibold mb-2">{item.content}</h3>
+                                <p className="text-white text-center px-4">
+                                    {item.description}
+                                </p>
+                            </div>
+                        </div>
+                    })}
+                </div>
+            </section>
 
 
-            </div>
-        </div>
-
-
+        </section>
     )
 }
 
